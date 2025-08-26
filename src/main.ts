421957f7eafd766313625a4d93dd4267331ab0b1
Gami19 <k.ikegami.15@gmail.com>
2025-08-26 23:57:21 +0900

    第1章をスタート

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
-console.log("Hello, Server! The development server is running.");
+import net from 'net';
+
+const PORT: number = 8080;
+const HOST: string = '127.0.0.1'
+
+// サーバーインスタンスの作成
+const server = net.createServer((socket) => {
+    // クライアントが接続したときの処理
+    console.log(`Client connected: ${socket.remoteAddress}:${socket.remotePort}`);
+    socket.on('data', (data: Buffer) => {
+        // クライアントからデータを受信したときの処理
+        console.log(`Received data: ${data.toString}`);
+        // クライアントにデータを送信
+        socket.write(`Echo: ${data}`);
+    });
+    // クライアントが切断したときの処理
+    socket.on('end',() => {
+        console.log(`Client disconnected: ${socket.remoteAddress}:${socket.remotePort}`);
+    })
+    // エラーハンドリング
+    socket.on('error',(err) => {
+        console.error(`Socket error: ${err.message}`);
+    })
+
+    // サーバーの開始（待ち状態）
+    server.listen(PORT,HOST,() => {
+        console.log(`Server listening on ${HOST}:${PORT}`);
+    })
+
+    server.on('error',(err) => {
+        // サーバーエラーハンドリング
+        console.error(`Server error: ${err.message}`);}
+
+    )
+});
